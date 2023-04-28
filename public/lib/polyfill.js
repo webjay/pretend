bf0eff2f9f41e6f6cafe7c43f8e1ca2446b57ef6
Jacob Friis Saxberg <jacob@webcom.dk>
2023-04-28 09:39:14 +0200

    use v0.4.1

--- public/lib/polyfill.js
+++ public/lib/polyfill.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/extensions, import/no-unresolved
-import AudioRecorder from 'https://cdn.jsdelivr.net/npm/audio-recorder-polyfill/index.js';
+import AudioRecorder from 'https://cdn.jsdelivr.net/npm/audio-recorder-polyfill@0.4.1/index.js';
 
 window.MediaRecorder = AudioRecorder;
